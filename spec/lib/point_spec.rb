12100141675d7516c8a1b6fe5c7d0ff48c5da90f
Sebastien Tanguy <seb@dthg.net>
2010-12-11 16:05:26 +0100

    Port distance computing to ruby

--- spec/lib/point_spec.rb
+++ spec/lib/point_spec.rb
@@ -6,7 +6,16 @@ describe Point do
     lambda { Point.new( 0, 0 ) }.should_not raise_error
   end
   describe 'dist' do
-    it 'should have a dist method'
-    it 'should return the distance between two points'
+    it 'should have a dist method' do 
+      p1 = Point.new( 0, 0 )
+      p2 = Point.new( 0, 0 )
+      lambda { p1.dist( p2 ) }.should_not raise_error
+    end
+    it 'should return the distance between two points' do 
+      p1 = Point.new( 48.111269, -1.667338 )
+      p2 = Point.new( 48.110836, -1.667760 )
+      expected_dist = 57.494
+      p1.dist( p2 ).floor.should == expected_dist.floor
+    end
   end
 end
