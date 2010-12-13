ac0f22d2475933844e0028469af69a49ec1da5d8
Sebastien Tanguy <seb@dthg.net>
2010-12-13 22:04:00 +0100

    Change data handling according to new format (Closes #5)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,24 +12,20 @@ jQuery.Star = {};
     }
     function onMarkerClick() {
         console.log("hello");
-        var directions = {};
-        for( var i = 0 ; i < this.times.length; ++i ) {
-            if ( directions[this.times[i].direction] === undefined ) {
-                directions[this.times[i].direction] = [];
-            }
-            directions[this.times[i].direction].push( this.times[i].time );
+        if ( this.times === undefined || this.times.length == 0 ) {
+            return;
         }
         var content = $("<div></div>").append(
             $("<h2></h2>").append(this.getTitle())
         );
-        $.each( directions, function( d, times ){
-            content.append( $("<h3></h3>").append( d ) );
+        for( var i = 0; i < this.times.length; ++i ) {
+            content.append( $("<h3></h3>").append( this.times[i].direction ) );
             var ul = $("<ul></ul>");
-            for( var i =0 ; i < times.length; ++i ) {
-                ul.append( $("<li></li>").append( times[i] ) );
+            for( var j = 0; j < this.times[i].times.length; ++j ) {
+                ul.append( $("<li></li>").append( this.times[i].times[j] ) );              
             }
             content.append( ul );
-        });
+        }
         var infow = new google.maps.InfoWindow({
             content: content[0]
         });
