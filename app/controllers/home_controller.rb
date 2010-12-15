9bc3fc7e889022eeff176e2ca247aa85afd819be
Sebastien Tanguy <seb@dthg.net>
2010-12-15 20:32:37 +0100

    Remove dead code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,18 +33,4 @@ class HomeController < ApplicationController
     end
     render :json => data
   end
-
-  def stop
-    line_id = params[:line]
-    stop_id = params[:stop]
-    now = Time.now
-    sts = StopTime.coming(line_id,stop_id).limit( 10 )
-    jsts = sts.collect do|stop_time|
-      { :time => stop_time.arrival.to_formatted_time,
-        :direction => stop_time.trip.headsign }
-    end
-    render :json => jsts
-  end
-      
-
 end
