0ad19e89c2a7831f5f32df0a1d6c93ed5da6484e
Sebastien Tanguy <stanguy@gmail.com>
2010-12-16 13:47:28 +0100

    Enhance the select of lines to regroup them

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,21 @@
+# encoding: utf-8
 module HomeHelper
+  def line_option line
+    attrs = { :value => line.id }
+    if line.fgcolor and line.bgcolor
+      attrs[:style] = "color: ##{line.fgcolor}; background-color: ##{line.bgcolor}"
+    end
+    content_tag :option, line.full_name, attrs
+  end
+  def line_options_for_select lines
+    lines_grouped = [ lines.select(&:is_urban?).collect {|l| line_option(l) }.join.html_safe,
+                      lines.select(&:is_suburban?).collect {|l| line_option(l) }.join.html_safe,
+                      lines.select(&:is_express?).collect {|l| line_option(l) }.join.html_safe,
+                      lines.select(&:is_special?).collect {|l| line_option(l) }.join.html_safe ]
+    titles_groups = [ "Lignes urbaines", "Lignes suburbaines",
+                      "Lignes express", "Lignes spéciales" ]
+    titles_groups.to_enum(:each_with_index).collect do |title,idx|
+      content_tag :optgroup, lines_grouped[idx], { :label => title }
+    end.join.html_safe
+  end
 end
--- app/models/line.rb
+++ app/models/line.rb
@@ -7,5 +7,22 @@ class Line < ActiveRecord::Base
 
   def full_name
     short_name + " " + long_name
-  end  
+  end
+  
+  def is_express?
+    if short_name.match(/^\d+$/)
+      num_id = short_name.to_i
+      return num_id.between?( 40, 49 ) || num_id.between?( 150, 200 ) 
+    end
+    [ "40ex", "KL" ].include? short_name
+  end
+  def is_suburban?
+    short_name.match(/^\d+$/) && short_name.to_i.between?( 50, 100 )
+  end
+  def is_urban?
+    short_name.match(/^\d+$/) && short_name.to_i.between?( 1, 39 )
+  end
+  def is_special?
+    ! ( is_express? || is_suburban? || is_urban? )
+  end
 end
