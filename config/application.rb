2b39204253e74706fd08a4e68f60995e7e49f04b
Sebastien Tanguy <seb@dthg.net>
2010-12-19 23:27:18 +0100

    Enable jQuery-UI

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,8 @@ module StarGtfs
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery.min rails jquery.colorbox)
-
+    config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery-ui.min rails jquery.colorbox)
+ 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
