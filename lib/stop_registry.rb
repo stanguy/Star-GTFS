9d7a4298ec2310838add311d346614f43109700f
Sebastien Tanguy <seb@dthg.net>
2011-06-02 18:48:16 +0200

    Add stop_alias

--- lib/stop_registry.rb
+++ lib/stop_registry.rb
@@ -13,7 +13,10 @@ class StopRegistry
     stop_name = clean_up( stop_name )
     key = stop_name.to_url
     unless @reg.has_key? key
-      @reg[key] = Stop.create( :name => stop_name )
+      @reg[key] = Stop.create( :name => stop_name, :accessible => false )
+      @reg[key].stop_aliases.create( :src_id => @reg[key].id,
+                                     :src_code => @reg[key].id,
+                                     :src_name => stop_name )
     end
     @reg[key]
   end    
--- spec/lib/stop_registry_spec.rb
+++ spec/lib/stop_registry_spec.rb
@@ -20,4 +20,7 @@ describe StopRegistry do
   it 'should not be case sensitive' do 
     @reg['Stop 1'].should == @reg['stop 1']    
   end
+  it 'should create a stop with one alias' do 
+    @reg['Stop 1'].stop_aliases.count.should == 1
+  end
 end
