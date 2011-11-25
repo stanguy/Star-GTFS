dfbaffd3d494a1686697d76beafb91dce38b77e9
Sebastien Tanguy <seb@dthg.net>
2011-11-26 00:50:54 +0100

    Add missing asset path

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module StarGtfs
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join( "app", "assets", "handheld" )
+
   end
 end
