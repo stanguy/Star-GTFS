313e33a2571c690065d5377ba23f09979cf7378f
Sebastien Tanguy <seb@dthg.net>
2012-01-27 00:08:50 +0100

    Static small-display version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,16 @@ module ApplicationHelper
     content_tag :div, '', :class => 'clear'
   end
       
+  MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+    'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+    'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+    'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+    'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+    'mobile'
+  
+  def is_mobile_device?
+    request.user_agent.to_s.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
+  end
+  
 
 end
