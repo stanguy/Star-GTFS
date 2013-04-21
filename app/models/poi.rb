be9e1330353ffdec26cb39919795c360283eb11d
Sebastien Tanguy <stanguy@gmail.com>
2013-04-21 07:32:13 -0700

    Fix proximity request, by casting geo objects

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -2,8 +2,8 @@ module POI
   def self.included(base)
     base.class_eval do  
       scope :close_to, lambda { |point,distance|
-        select( "*,st_distance( Transform( geom, 900913 ), transform( ST_GeomFromText('POINT(#{point.x} #{point.y})', 4326), 900913)) as distance").
-        where( "ST_DWithin(transform( geom, 900913 ), transform( ST_GeomFromText('POINT(? ?)', ?), 900913), ?)",
+        select( "*,postgis.st_distance( Transform( geom::geometry, 900913 ), transform( ST_GeomFromText('POINT(#{point.x} #{point.y})', 4326), 900913)) as distance").
+        where( "postgis.ST_DWithin(transform( geom::geometry, 900913 ), transform( ST_GeomFromText('POINT(? ?)', ?), 900913), ?)",
                point.x, point.y, 4326, distance ).order( "distance ASC" )
       }
     end
